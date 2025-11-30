3ae2c6076acffe435021787a3bc5835318bb49b8
robotoloco3 <roland@shikoski.com>
2025-11-29 22:47:21 -0600

    Fix crashing.

--- src/main/java/com/king_tajin/repo_love_potion/events/ChargedCreeperDropHandler.java
+++ src/main/java/com/king_tajin/repo_love_potion/events/ChargedCreeperDropHandler.java
@@ -6,13 +6,13 @@ import net.minecraft.world.entity.monster.Skeleton;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
-import com.king_tajin.repo_love_potion.item.ILoveDiscItem;
+import com.king_tajin.repo_love_potion.init.RepoLovePotionItems;
+import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
 
 public class ChargedCreeperDropHandler {
 
     @SubscribeEvent
-    public static void onEntityDeath(LivingDeathEvent event) {
+    public static void onEntityDrops(LivingDropsEvent event) {
         if (!(event.getEntity() instanceof Creeper creeper)) {
             return;
         }
@@ -23,12 +23,18 @@ public class ChargedCreeperDropHandler {
 
         if (event.getSource().getDirectEntity() instanceof Arrow arrow) {
             if (arrow.getOwner() instanceof Skeleton) {
+                event.getDrops().clear();
 
-                ItemStack musicDisc = new ItemStack(new ILoveDiscItem());
-                creeper.spawnAtLocation(musicDisc);
-
-                ItemStack rose = new ItemStack(Items.POPPY);
+                ItemStack rose = new ItemStack(Items.POPPY, 1);
                 creeper.spawnAtLocation(rose);
+
+                ItemStack gunpowder = new ItemStack(Items.GUNPOWDER, 3);
+                creeper.spawnAtLocation(gunpowder);
+
+                if (creeper.level().random.nextFloat() < 0.4f) {
+                    ItemStack musicDisc = new ItemStack(RepoLovePotionItems.I_LOVE_DISC.get());
+                    creeper.spawnAtLocation(musicDisc);
+                }
             }
         }
     }
